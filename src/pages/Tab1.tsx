8f08973b01d0d61b127cd1ba1b5c738a6111a3b1
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-02 20:34:37 +0200

    Se crea un archivo data con datos ficticios y el archivo de tipos index.ts con el tipo para el archivo data. Se maqueta Tab1.tsx con un grid y se muestran los datos de los libros. Se incorpora una barra de busqueda

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar,  IonCard, IonImg, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonIcon, IonCol, IonRow, IonGrid } from '@ionic/react';
+import { search, repeat, cart } from 'ionicons/icons';
+import { data } from '../data/data';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+
+  const exchangeIcon = <IonIcon icon={repeat}>Intercambio</IonIcon>
+  const cartIcon = <IonIcon icon={cart}>Venta</IonIcon>
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Libros cerca de ti</IonTitle>
+        </IonToolbar>
+        <IonToolbar>
+          <IonSearchbar searchIcon={search} placeholder="Buscar por título o autor"></IonSearchbar>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonGrid>
+          <IonRow>
+            {data.map((item) => (
+              <IonCol size="6" size-md="4" size-lg="2" key={item.id}>
+                <IonCard>
+                  <IonImg
+                    src={item.image}
+                  />
+                  <IonCardHeader>
+                    <IonCardSubtitle>{item.author}</IonCardSubtitle>
+                    <IonCardTitle>{item.title}</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent className="price">
+                    {item.price} € - {item.exchange ? exchangeIcon  : cartIcon }
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Data = {
+    id: number;
+    title: string;
+    author: string;
+    description: string;
+    price: number;
+    image: string;
+    exchange: boolean;
+    location: string;
+}
