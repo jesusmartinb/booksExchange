13f5fce22b5c7e696072f4e55ff0a844e4d6cb40
Jesús Martín Blanco <jesusmartinb64@gmail.com>
2024-04-02 21:04:30 +0200

    Se implementa debounce search por titulo o autor. Se crea el archivo .nvmrc

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar,  IonCard, IonImg, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonIcon, IonCol, IonRow, IonGrid } from '@ionic/react';
 import { search, repeat, cart } from 'ionicons/icons';
 import { data } from '../data/data';
@@ -10,6 +10,16 @@ const Tab1: React.FC = () => {
   const exchangeIcon = <IonIcon icon={repeat}>Intercambio</IonIcon>
   const cartIcon = <IonIcon icon={cart}>Venta</IonIcon>
 
+  let [results, setResults] = useState([...data]);
+
+  const handleInput = (e: Event) => {
+    let query = '';
+    const target = e.target as HTMLIonSearchbarElement;
+    if (target) query = target.value!.toLowerCase();
+
+    setResults(data.filter((d) => d.title.toLowerCase().indexOf(query) > -1 || d.author.toLowerCase().indexOf(query) > -1));
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,13 +27,13 @@ const Tab1: React.FC = () => {
           <IonTitle>Libros cerca de ti</IonTitle>
         </IonToolbar>
         <IonToolbar>
-          <IonSearchbar searchIcon={search} placeholder="Buscar por título o autor"></IonSearchbar>
+          <IonSearchbar searchIcon={search} placeholder="Buscar por título o autor" debounce={1000} onIonInput={(e) => handleInput(e)}></IonSearchbar>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonGrid>
           <IonRow>
-            {data.map((item) => (
+            {results.map((item) => (
               <IonCol size="6" size-md="4" size-lg="2" key={item.id}>
                 <IonCard>
                   <IonImg
